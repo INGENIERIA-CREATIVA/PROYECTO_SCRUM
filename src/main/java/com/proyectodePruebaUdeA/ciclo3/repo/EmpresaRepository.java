3ffb2d2863f9d032d100897123c0f025b67cfd27
JUAN DIEGO CANO MEJIA <know1988@hotmail.com>
2022-09-14 15:51:50 -0500

    Ajuste menor

--- src/main/java/com/proyectodePruebaUdeA/ciclo3/repo/EmpresaRepository.java
+++ src/main/java/com/proyectodePruebaUdeA/ciclo3/repo/EmpresaRepository.java
@@ -4,7 +4,7 @@ import com.proyectodePruebaUdeA.ciclo3.modelos.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository //Anotacion que le dice a Sprint que esta clase es un repositorio
+@Repository //Anotacion que le dice a Spring que esta clase es un repositorio
 public interface EmpresaRepository extends JpaRepository<Empresa, Integer>{
 
 }
